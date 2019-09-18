25cb7d0f2b9f7f081001e3855eb80461b6b44131
elyeel <elyeel@yahoo.com>
2019-09-18 23:14:26 +0000

    added sold-out feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,7 +55,7 @@ cat1.products.create!({
   name:  'Hipster Hat',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel3.jpg'),
-  quantity: 4,
+  quantity: 0, # was 4
   price: 34.49
 })
 
@@ -104,7 +104,7 @@ cat2.products.create!({
   name:  'World\'s Largest Smartwatch',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('electronics3.jpg'),
-  quantity: 32,
+  quantity: 0, # was 32
   price: 2_026.29
 })
 
@@ -120,7 +120,7 @@ cat3.products.create!({
   name:  'Electric Chair',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture2.jpg'),
-  quantity: 2,
+  quantity: 0, # was 2
   price: 987.65
 })
 
