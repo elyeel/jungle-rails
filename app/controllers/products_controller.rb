02c99f470784205948c7857e4e8652de7b3d6b7a
elyeel <elyeel@yahoo.com>
2019-09-18 18:27:47 +0000

    started working @fix/moneyformatting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # View
+    # render :index implicitly called by similarity in method's name
   end
 
   def show
