1286ee02dc2dd0e47fb163e2c7af80d9103dc774
elyeel <elyeel@yahoo.com>
2019-09-24 03:04:41 +0000

    added validation to users model, working on rspec on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
+  PASSWORD_REQUIREMENTS = /\A
+    (?=.{6,})
+  /x
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, format: PASSWORD_REQUIREMENTS
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+
+  end
 end
