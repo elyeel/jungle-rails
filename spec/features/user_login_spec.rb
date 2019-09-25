2de75821e636bd52f91caee40396b3fd771bb326
elyeel <elyeel@yahoo.com>
2019-09-25 02:26:44 +0000

    user login spec done

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @user = User.create!( 
+      first_name: "Anything",
+      last_name: "Something",
+      email: "my@email.com",
+      password: "abcdef",
+      password_confirmation: "abcdef"
+    )
+    
+  end
+
+  scenario "They see a products" do
+    # ACT
+    visit root_path
+    visit "/login"
+    fill_in('email', :with => "my@email.com")
+    fill_in("password", :with => "abcdef")
+    click_button "Submit"
+
+
+
+    # DEBUG / VERIFY
+    sleep 5
+    save_screenshot
+
+    expect(page).to have_content 'my@email.com'
+  end
+
+end
